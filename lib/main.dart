7b210d3d8afba351a6699d9d196b844fadf5fd0c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-04 20:35:33 +0530

    wrap the common code present inside the onPressed property of each FlatButton() widget inside a new function called playSound()

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound() {
+    final player = AudioCache();
+    player.play('note1.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,8 +20,7 @@ class XylophoneApp extends StatelessWidget {
               FlatButton(
                 color: Colors.red,
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
+                  playSound();
                 },
                 child: null,
               ),
